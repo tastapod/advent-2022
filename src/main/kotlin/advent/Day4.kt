1459f19f9a418af88c564bc83fe3c8389f3d090f
Daniel Terhorst-North <daniel@dannorth.net>
2022-12-13 00:16:41 +0100

    resolve IntelliJ warnings

--- src/main/kotlin/advent/Day4.kt
+++ src/main/kotlin/advent/Day4.kt
@@ -1,8 +1,7 @@
 package advent
 
 object Day4 {
-    private fun IntRange.containsRange(other: IntRange) =
-        other.first >= first && other.last <= last
+    private fun IntRange.containsRange(other: IntRange) = other.first >= first && other.last <= last
 
     private fun IntRange.overlaps(other: IntRange) =
         containsRange(other) || contains(other.first) || contains(other.last)
@@ -10,8 +9,8 @@ object Day4 {
     fun fullyContained(ranges: Pair<IntRange, IntRange>) =
         ranges.first.containsRange(ranges.second) || ranges.second.containsRange(ranges.first)
 
-    fun parseAssignments(assignmentPair: String) = assignmentPair.split(",").map {
-        it.split("-").map {
+    fun parseAssignments(assignmentPair: String) = assignmentPair.split(",").map { range ->
+        range.split("-").map {
             it.toInt()
         }.let {
             it[0]..it[1]
@@ -23,8 +22,7 @@ object Day4 {
     fun countFullyContainedRanges(assignments: List<String>) =
         assignments.map { parseAssignments(it) }.count { fullyContained(it) }
 
-    fun overlaps(ranges: Pair<IntRange, IntRange>) =
-        ranges.first.overlaps(ranges.second)
+    fun overlaps(ranges: Pair<IntRange, IntRange>) = ranges.first.overlaps(ranges.second)
 
     fun countOverlappingRanges(assignments: List<String>) =
         assignments.map { parseAssignments(it) }.count { it.first.overlaps(it.second) || it.second.overlaps(it.first) }
