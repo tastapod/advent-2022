72015ce731fa2ecc14572fad0edb1e8e127436e9
Daniel Terhorst-North <daniel@dannorth.net>
2022-12-13 23:37:59 +0100

    solve Day 5 part 2, much more quickly!

--- src/test/kotlin/advent/Day5Test.kt
+++ src/test/kotlin/advent/Day5Test.kt
@@ -69,7 +69,45 @@ class Day5Test {
         )
 
         Day5.moveCrates9000(stacksByLabel, moves)
-
         assertEquals(expected, stacksByLabel)
     }
+
+    @Test
+    fun `moves crates in bulk`() {
+        val stacks = mapOf(
+            stack('1', 'A', 'B'),
+            stack('2', 'C'),
+        )
+
+        val expected = mapOf(
+            stack('1'),
+            stack('2', 'C', 'A', 'B'),
+        )
+
+        stacks.moveCrates9001(2, '1', '2')
+
+        assertEquals(expected, stacks)
+    }
+
+    @Test
+    fun `moves sample crates in bulk`() {
+        val stacksByLabel = Day5.parseDrawing(sampleDrawing)
+
+        val moves = """
+            move 1 from 2 to 1
+            move 3 from 1 to 3
+            move 2 from 2 to 1
+            move 1 from 1 to 2
+        """.trimIndent().split("\n")
+
+        val expected = mapOf(
+            stack('1', 'M'),
+            stack('2', 'C'),
+            stack('3', 'P', 'Z', 'N', 'D')
+        )
+
+        Day5.moveCrates9001(stacksByLabel, moves)
+        assertEquals(expected, stacksByLabel)
+    }
+
 }
