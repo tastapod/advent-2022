2cd772d1c7a0aafb1f1cfb338194398b4530d044
Daniel Terhorst-North <daniel@dannorth.net>
2022-12-14 23:17:06 +0100

    add some explanation

--- src/main/kotlin/advent/Day6.kt
+++ src/main/kotlin/advent/Day6.kt
@@ -3,8 +3,11 @@ package advent
 object Day6 {
     fun findStartOfPacket(sequence: String) = findMarker(sequence, 4)
 
-    private fun findMarker(sequence: String, size: Int) =
-        sequence.windowed(size).takeWhile { it.toSet().size < size }.count() + size
+    /**
+     * Use the fact that a sequence of chars is the same size as a set of the chars only if they are all different
+     */
+    private fun findMarker(sequence: String, length: Int) =
+        sequence.windowed(length).takeWhile { it.toSet().size < length }.count() + length
 
     fun findStartOfMessage(sequence: String) = findMarker(sequence, 14)
 }
