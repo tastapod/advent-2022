d6504a886b6bb4744d51d94cc8db9b967f56d613
Daniel Terhorst-North <daniel@dannorth.net>
2022-12-19 10:25:31 +0000

    solve Day 7 part 2 by introducing Filesystem class

--- src/main/kotlin/advent/day7/Filesystem.kt
+++ src/main/kotlin/advent/day7/Filesystem.kt
@@ -6,13 +6,18 @@ sealed class Entry(open val name: String) {
 
 data class File(override val name: String, override val size: Int) : Entry(name)
 
-data class Dir(override val name: String, var contents: MutableSet<Entry> = mutableSetOf()) : Entry(name) {
+data class Dir(override val name: String, var contents: MutableSet<Entry> = mutableSetOf(), val capacity: Int) :
+    Entry(name) {
     /**
      * These shenanigans are necessary to avoid the link to parent directory causing a stack overflow in `hashCode` etc.
      *
      * There may be a better way to exclude a data class property from `equals`, `hashCode`, `toString`
      */
-    constructor(name: String, up: Dir? = null, contents: MutableSet<Entry> = mutableSetOf()) : this(name, contents) {
+    constructor(
+        name: String,
+        up: Dir? = null,
+        contents: MutableSet<Entry> = mutableSetOf(),
+        capacity: Int = 70_000_000) : this(name, contents, capacity) {
         this.up = up
     }
 
@@ -24,8 +29,22 @@ data class Dir(override val name: String, var contents: MutableSet<Entry> = muta
         contents.sumOf { entry -> entry.size }
     }
 
-    private fun walkDirectories(): Iterable<Dir> =
+    fun walkDirectories(): Iterable<Dir> =
         contents.filterIsInstance<Dir>().flatMap { it.walkDirectories() } + this
+}
 
-    fun sumDirectoriesBelowSize(limit: Int) = walkDirectories().filter { it.size <= limit }.sumOf { it.size }
+data class Filesystem(val root: Dir = Dir("/"), val capacity: Int = DEFAULT_CAPACITY) {
+    fun sumDirectoriesBelowSize(limit: Int) = root.walkDirectories().filter { it.size <= limit }.sumOf { it.size }
+    companion object {
+        const val DEFAULT_CAPACITY = 70_000_000
+    }
+
+    fun smallestDirToEnsureSpace(spaceRequired: Int): Dir {
+        val spaceNeeded: Int
+        // capacity = 1000, used = 400, required = 700
+        // spaceNeeded = 700 - (1000 - 400)
+        return root.walkDirectories().sortedByDescending { it.size }
+            .also { spaceNeeded = spaceRequired + it.first().size - capacity }
+            .windowed(2).find {it[0].size >= spaceNeeded && it[1].size < spaceNeeded}!!.first()
+    }
 }
