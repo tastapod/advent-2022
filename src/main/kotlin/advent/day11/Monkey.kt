a9b4fdd49647ac9184b956a393e6dc21e7d6c9e3
Daniel Terhorst-North <daniel@dannorth.net>
2022-12-28 19:39:08 +0000

    solve Day 11 part 2 using a neato GCD/LCM algorithm from SO - convert `Monkeys` to just be extensions on `List<Monkey>`

--- src/main/kotlin/advent/day11/Monkey.kt
+++ src/main/kotlin/advent/day11/Monkey.kt
@@ -1,23 +1,26 @@
 package advent.day11
 
-typealias ItemList = ArrayDeque<Int>
+typealias ItemList = ArrayDeque<Long>
 typealias Receivers = Map<Boolean, Int>
+typealias Monkeys = List<Monkey>
 
-data class Throw(val item: Int, val targetMonkey: Int)
+data class Throw(val item: Long, val targetMonkey: Int)
 
+data class Monkey(
+    val index: Int, val items: ItemList, val operation: Operation, val divisor: Long, val receivers: Receivers) {
 
-class Monkey(
-    val index: Int, items: List<Int>, val operation: Operation, val divisor: Int, val receivers: Receivers) {
+    constructor(index: Int, items: List<Long>, operation: Operation, divisor: Long, receivers: Receivers) :
+            this(index, ItemList(items), operation, divisor, receivers)
 
     var inspections: Int = 0
-    val items = ItemList(items)
 
     fun inspectNextItem() = inspectItem(items.removeFirst())
-    fun takeTurn() = items.map { inspectItem(it) }.also { items.clear() }
 
-    private fun inspectItem(item: Int) = (operation(item) / 3).let {
+    fun takeTurn(worried: Boolean = true) = items.map { inspectItem(it, worried) }.also { items.clear() }
+
+    private fun inspectItem(item: Long, worried: Boolean = true) = (operation(item) / if (worried) 3 else 1).let {
         inspections++
-        Throw(it, receivers[it % divisor == 0]!!)
+        Throw(it, receivers[it % divisor == 0L]!!)
     }
 
     companion object {
@@ -31,34 +34,53 @@ class Monkey(
          *         If true: throw to monkey 2
          *         If false: throw to monkey 0
          */
-        fun parse(note: String): Monkey {
+        fun parseNote(note: String): Monkey {
             fun List<String>.trailingInt(prefix: String) =
                 this.first { it.startsWith(prefix) }.run { split(" ").last().toInt() }
 
             val lines = note.lines().map { it.trim() }
 
-            val index =
-                lines.first { it.startsWith("Monkey") }.run { split(' ', ':')[1].toInt() }
-            val items =
-                lines.first { it.startsWith("Starting items") }.run { split(": ")[1].split(", ").map { it.toInt() } }
-            val operation =
-                lines.first { it.startsWith("Operation") }.run { parseOperation(split(": ")[1]) }
-            val divisor = lines.trailingInt("Test:")
-            val receivers: Receivers = mapOf(
-                true to lines.trailingInt("If true"),
-                false to lines.trailingInt("If false")
-            )
+            return Monkey(
 
-            return Monkey(index, items, operation, divisor, receivers)
+                lines.first { it.startsWith("Monkey") }.run { split(' ', ':')[1].toInt() },
+                lines.first { it.startsWith("Starting items") }.run { split(": ")[1].split(", ").map { it.toLong() } },
+                lines.first { it.startsWith("Operation") }.run { parseOperation(split(": ")[1]) },
+                lines.first { it.startsWith("Test:") }.run { split(" ").last().toLong() },
+                mapOf(
+                    true to lines.trailingInt("If true"),
+                    false to lines.trailingInt("If false")
+                ))
         }
+
+        fun parseNotes(notes: String) = notes.split("\n\n").map { parseNote(it) }
+            .makeOperationResultsModuloTheLcmOfTestDivisors()
 
         fun parseOperation(operation: String): Operation {
             val (op, arg) = operation.split(" ").takeLast(2)
             return when (op) {
-                "+" -> if (arg == "old") PlusSelf() else Plus(arg.toInt())
-                "*" -> if (arg == "old") TimesSelf() else Times(arg.toInt())
+                "+" -> if (arg == "old") PlusSelf() else Plus(arg.toLong())
+                "*" -> if (arg == "old") TimesSelf() else Times(arg.toLong())
                 else -> throw IllegalArgumentException("Unknown operation: $operation")
             }
         }
     }
 }
+
+fun Monkeys.makeOperationResultsModuloTheLcmOfTestDivisors(): List<Monkey> {
+    // from https://stackoverflow.com/a/49722579
+    fun gcd(a: Long, b: Long): Long = if (a > 0) gcd(b % a, a) else b
+    fun lcm(a: Long, b: Long) = a * b / gcd(a, b)
+
+    val modulus = map { it.divisor }.reduce(::lcm)
+
+    return map { it.copy(operation = Modulo(modulus, it.operation)) }
+}
+
+fun Monkeys.playRound(worried: Boolean = true) {
+    forEach { monkey ->
+        monkey.takeTurn(worried).forEach { this[it.targetMonkey].items.add(it.item) }
+    }
+}
+
+fun Monkeys.monkeyBusiness() =
+    sortedByDescending { it.inspections }.take(2).let { (m1, m2) -> m1.inspections.toLong() * m2.inspections.toLong() }
--- src/main/kotlin/advent/day11/Operation.kt
+++ src/main/kotlin/advent/day11/Operation.kt
@@ -1,19 +1,23 @@
 package advent.day11
 
-typealias Operation = (Int) -> Int
+typealias Operation = (Long) -> Long
 
 class PlusSelf : Operation {
-    override fun invoke(old: Int) = old + old
+    override fun invoke(old: Long) = old + old
 }
 
-class Plus(private val arg: Int) : Operation {
-    override fun invoke(old: Int) = old + arg
+class Plus(private val arg: Long) : Operation {
+    override fun invoke(old: Long) = old + arg
 }
 
 class TimesSelf : Operation {
-    override fun invoke(old: Int) = old * old
+    override fun invoke(old: Long) = old * old
 }
 
-class Times(private val arg: Int) : Operation {
-    override fun invoke(old: Int) = old * arg
+class Times(private val arg: Long) : Operation {
+    override fun invoke(old: Long) = old * arg
+}
+
+class Modulo(private val modulus: Long, private val operation: Operation): Operation {
+    override fun invoke(old: Long) = operation(old) % modulus
 }
